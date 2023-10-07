6442c16674a1fcb6585bfc4d9be7d10b6bd33ec3
Momo0407 <katsuki.i0407@icloud.com>
2023-10-07 09:53:30 +0900

    TextMeshProを導入しました

---
+++ Assets/Scripts/Typing/PlayerTyping.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// プレイヤーのタイピングの入力を受け付ける
+/// </summary>
+public class PlayerTyping : MonoBehaviour
+{
+    //TMPro.TMP_Text text;
+    TextMesh text;
+    
+    void Start()
+    {
+        StartCoroutine(PlayerLoop());
+        print(text.text[0]);
+    }
+
+   　IEnumerator PlayerLoop()
+    {
+        while (true)
+        {
+            yield return new WaitUntil(() => Input.anyKeyDown);
+
+            var key = Input.inputString;    //キーボード入力を読み取り 
+        }
+    }
+}
