090bc08615e9a2bbc3cb716661e827c106a60587
Ivan Latypov <paramorphling@gmail.com>
2023-10-07 10:21:58 +0900

    Dictionaryシーンを追加

---
+++ Assets/Scripts/DictionaryScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictionaryScene : AdditiveSceme
+{
+    void Start()
+    {
+        Debug.Log("辞書起動");
+    }
+}
--- Assets/Scripts/MainScene.cs
+++ Assets/Scripts/MainScene.cs
@@ -19,6 +19,8 @@ public class MainScene : MonoBehaviour
         yield return new WaitForSeconds(0.2f);
         SceneManager.LoadScene("UI", LoadSceneMode.Additive);
         yield return new WaitForSeconds(0.2f);
+        SceneManager.LoadScene("Dictionary", LoadSceneMode.Additive);
+        yield return new WaitForSeconds(0.2f);
         SceneManager.LoadScene("Typing", LoadSceneMode.Additive);
         yield return new WaitForSeconds(0.2f);
         SceneManager.LoadScene("Gimmick", LoadSceneMode.Additive);
