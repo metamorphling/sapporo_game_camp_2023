e471893d6aaf58b5d73f1a1d0c5cc0a7985b27a1
Momo0407 <katsuki.i0407@icloud.com>
2023-10-07 14:10:53 +0900

    文字オブジェクトとプレイヤーの機能を分離しました

--- Assets/Scripts/Typing/PlayerTyping.cs
+++ Assets/Scripts/Typing/PlayerTyping.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,35 +11,130 @@ public class PlayerTyping : MonoBehaviour
 {
     [SerializeField] TMPro.TMP_Text text;
 
-    public int Score {  get; private set; }
+    [SerializeField] TypingWordBase word;
 
-    void Start()
+    public int Score { get; private set; } = 0;
+
+    public event System.Action OnPlayerCorrectlyTypedCallBack;
+    public event System.Action OnPlayerMissTypedCallBack;
+
+    Queue<char> inputKeysQueue = new(0);
+    IEnumerator Start()
     {
-        StartCoroutine(PlayerLoop());
-        print(text.text.ToLower()[text.firstVisibleCharacter]);
+        StartCoroutine(KeyMatchingLoop());
+        yield return PlayerInputLoop();
     }
 
-    IEnumerator PlayerLoop()
+    /// <summary>
+    /// プレイヤーの入力を処理するループ
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator PlayerInputLoop()
     {
+
+
         while (true)
         {
-            yield return new WaitUntil(() => Input.anyKeyDown & Input.inputString.Length > 0);
+            yield return null;
+            yield return WaitForKeyBoardInput(); //キーボード入力を待つ
 
-            var key = Input.inputString;    //キーボード入力を読み取り
+            //var key = ReadKeyBoardInput();   //キーボード入力を読み取り
 
-            print(key.Length);
+            EnQueueKeyBoardInput();
 
-            if (key[0] == text.text.ToLower()[text.firstVisibleCharacter])
+            /*
+            if (text.text == null || text.text.Length==0)
+            {
+                continue;
+            }
+            
+            if (key == text.text.ToLower()[text.firstVisibleCharacter])
             {
                 text.text = text.text.Remove(text.firstVisibleCharacter, 1);
+
+                PlayerCorrectlyTyped();
             }
             else
             {
-                print("miss");
-                //Score--;
+                PlayerMissTyped();
             }
+            
 
-            yield return null;
+            static char ReadKeyBoardInput()
+            {
+                return Input.inputString[0];
+            }//InputStringをcharに変換
+            */
+
+            void EnQueueKeyBoardInput()
+            {
+                var array = Input.inputString.ToCharArray();
+                foreach(var c in array)
+                {
+                    print(c);
+                    inputKeysQueue.Enqueue(c);
+                }
+            }
         }
+    }
+
+    IEnumerator KeyMatchingLoop()
+    {
+        while (true)
+        {
+            yield return new WaitUntil(() => inputKeysQueue.Count != 0);
+
+            while(inputKeysQueue.Count > 0)
+            {
+                yield return null;
+
+                var key = inputKeysQueue.Dequeue();
+
+                print(key);
+
+                word.SendTypingCharacter(key, out bool matched);
+
+                if (matched)
+                {
+                    PlayerCorrectlyTyped();
+                }
+                else
+                {
+                    PlayerMissTyped();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// キーボード入力を待つ
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator WaitForKeyBoardInput()
+    {
+        yield return new WaitUntil(() => Input.anyKeyDown & Input.inputString != null & Input.inputString.Length > 0);
+    }
+
+    /// <summary>
+    /// スコアをリセット
+    /// </summary>
+    public void ResetScore() => Score = 0;
+
+    /// <summary>
+    /// プレイヤーが正しいキーを押したとき
+    /// </summary>
+    void PlayerCorrectlyTyped()
+    {
+        OnPlayerCorrectlyTypedCallBack?.Invoke();
+        //print("correct");
+    }
+
+    /// <summary>
+    /// プレイヤーが間違えたキーを押したとき
+    /// </summary>
+    void PlayerMissTyped()
+    {
+        OnPlayerMissTypedCallBack?.Invoke();
+        //print("miss");
     }
 }
