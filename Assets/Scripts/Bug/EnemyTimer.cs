b3aef82d901bf84e4925377b93fc633b67b75a52
KifuaMoon <kifuyuoo@gmail.com>
2023-10-08 11:55:30 +0900

    バグ修正

--- Assets/Scripts/Bug/EnemyTimer.cs
+++ Assets/Scripts/Bug/EnemyTimer.cs
@@ -38,14 +38,6 @@ public class EnemyTimer : MonoBehaviour
         {
             int b = bugs.Length;
             int r = Random.Range(0, b);
-            if(r == 2 || r == 3)
-            {
-                bugs[r].Initialize(10);
-            }
-            else
-            {
-                bugs[r].Initialize(5);
-            }
             bug1Time = 0;
         }
         //バグが出てなかったら
